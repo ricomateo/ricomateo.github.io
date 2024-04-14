09b09820a8c177767f8364f6131af19f0439d9b0
ricomateo <mrico@fi.uba.ar>
2024-04-14 00:13:29 -0300

    arreglo los botones del header para poder usarlos al estar parado en el proyecto

--- docs/portfolio.js
+++ docs/portfolio.js
@@ -1,132 +1,153 @@
-
-
 function sleep(ms) {
-    setTimeout(() => {}, ms);
+  setTimeout(() => {}, ms);
 }
-  
 
 function setUpRunning() {
-    let button = document.querySelector('.run-button');
-    let terminal = document.querySelector('.console-content');
-    let span = document.createElement('span');
-    let p = document.createElement('p');
-    span.classList.add("comment");
-    span.textContent = '/home/portfolio';
-    p.appendChild(span);
-    p.append(":~$ ./portfolio");
-    terminal.appendChild(p);
-    button.textContent = "Running!";
-    button.style.backgroundColor = "green";
+  let button = document.querySelector(".run-button");
+  let terminal = document.querySelector(".console-content");
+  let span = document.createElement("span");
+  let p = document.createElement("p");
+  span.classList.add("comment");
+  span.textContent = "/home/portfolio";
+  p.appendChild(span);
+  p.append(":~$ ./portfolio");
+  terminal.appendChild(p);
+  button.textContent = "Running!";
+  button.style.backgroundColor = "green";
 
-    let body = document.querySelector('body');
-    let container = document.querySelector('.container');
-    sleep(10000);
-    body.removeChild(container);
+  let body = document.querySelector("body");
+  let container = document.querySelector(".container");
+  sleep(10000);
+  body.removeChild(container);
 }
 // cuando clickeo en un boton, se le agrega la clase selected
 // y se le saca el selected al boton que estaba selected antes.
 function setUpButtons() {
-    let buttons = document.querySelectorAll('.button');
-    let aboutMeSection = document.querySelector('.about-me-container');
-    let projectsSection = document.querySelector('.projects');
-    let homeSection = document.querySelector('.presentation');
-    let contactSection = document.querySelector('.contact');
+  let buttons = document.querySelectorAll(".button");
+  let aboutMeSection = document.querySelector(".about-me-container");
+  let projectsSection = document.querySelector(".projects-container");
+  let homeSection = document.querySelector(".presentation");
+  let contactSection = document.querySelector(".contact");
+  let wormsProject = document.querySelector(".worm");
+  let container = document.querySelector(".container");
+  console.log("buttons = ", buttons);
+  buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+      aboutMeSection.classList.remove("hidden");
+      projectsSection.classList.remove("hidden");
+      homeSection.classList.remove("hidden");
+      contactSection.classList.remove("hidden");
+      wormsProject.classList.add("hidden");
+      container.classList.remove("project-holder");
+      let previouslySelected = document.querySelector(".selected");
+      if (previouslySelected != null) {
+        previouslySelected.classList.remove("selected");
+      }
+      button.classList.add("selected");
 
-    // location.assign("index.html");
-    // window.location.href = "index.html";
-    console.log("buttons = ", buttons);
-    buttons.forEach((button) => {
-        button.addEventListener('click', () => {
-            console.log("clickeaste en ", button);
-            let previouslySelected = document.querySelector('.selected');
-            if (previouslySelected != null) {
-                previouslySelected.classList.remove('selected');
-            }
-            button.classList.add('selected');
-
-            if (button.classList.contains('about-me-button')) {
-                scrollToSection(aboutMeSection);
-            } else if (button.classList.contains('projects-button')) {
-                scrollToSection(projectsSection);
-            } else if (button.classList.contains('home')) {
-                scrollToSection(homeSection);
-            } else if (button.classList.contains('contact-button')) {
-                scrollToSection(contactSection);
-            }
-        })
+      if (button.classList.contains("about-me-button")) {
+        scrollToSection(aboutMeSection);
+      } else if (button.classList.contains("projects-button")) {
+        scrollToSection(projectsSection);
+      } else if (button.classList.contains("home")) {
+        scrollToSection(homeSection);
+      } else if (button.classList.contains("contact-button")) {
+        scrollToSection(contactSection);
+      }
     });
+  });
 }
 
 function replace(string, index, replacement) {
-    return string.substring(0, index) + replacement + string.substring(index + replacement.length);
+  return (
+    string.substring(0, index) +
+    replacement +
+    string.substring(index + replacement.length)
+  );
 }
 
 function setUpName() {
-    let name = document.querySelector('.title');
-    name.textContent = "                   ";
-    let text = "Software developer";
+  let name = document.querySelector(".title");
+  name.textContent = "                   ";
+  let text = "Software developer";
 
-    let i = 0;
-    let string = name.textContent;
-    let writing = true;
-    const interval = setInterval(() => {
-        string = replace(string, i, text[i]);
-        string = replace(string, i + 1, "█");
-        name.textContent = string;
-        i++;
-        if (i === text.length) {
-            clearInterval(interval);
-            writing = false;
-        }
-        titilate(writing);
-    }, 150);
-    
+  let i = 0;
+  let string = name.textContent;
+  let writing = true;
+  const interval = setInterval(() => {
+    string = replace(string, i, text[i]);
+    string = replace(string, i + 1, "█");
+    name.textContent = string;
+    i++;
+    if (i === text.length) {
+      clearInterval(interval);
+      writing = false;
+    }
+    titilate(writing);
+  }, 150);
 }
 
 function titilate(writing) {
-    if (writing) {
-        console.log("still writing");
-        return;
+  if (writing) {
+    console.log("still writing");
+    return;
+  }
+  console.log("no more writing");
+  let name = document.querySelector(".title");
+  let j = 0;
+  const interval2 = setInterval(() => {
+    if (j % 2 == 0) {
+      name.textContent = "Software developer";
+    } else {
+      name.textContent = "Software developer█";
     }
-    console.log("no more writing");
-    let name = document.querySelector('.title');
-    let j = 0;
-    const interval2 = setInterval(() => {
-        if (j % 2 == 0) {
-            name.textContent = "Software developer";
-        } else {
-            name.textContent = "Software developer█";
-        }
-        j++;
-        if (j === -1) {
-            clearInterval(interval2);
-        }
-    }, 500);
+    j++;
+    if (j === -1) {
+      clearInterval(interval2);
+    }
+  }, 500);
 }
 
 function delay() {
-    let container = document.querySelector('.container');
-    let body = document.querySelector('body');
-    body.removeChild(container);
-    let j = 0;
-    const interval = setInterval(() => {
-        if (j === 1) {
-            clearInterval(interval);
-            body.appendChild(container);
-        }
-        j++;
-    }, 2000);
-
+  let container = document.querySelector(".container");
+  let body = document.querySelector("body");
+  body.removeChild(container);
+  let j = 0;
+  const interval = setInterval(() => {
+    if (j === 1) {
+      clearInterval(interval);
+      body.appendChild(container);
+    }
+    j++;
+  }, 2000);
 }
 
 function scrollToSection(section) {
-    section.scrollIntoView({ behavior: "smooth", block: "center" });   
+  section.scrollIntoView({ behavior: "smooth", block: "center" });
 }
 
-function setScrollToAbout() {
+function setUpProjectButton() {
+  let projectButton = document.querySelector(".project");
+  let wormsProject = document.querySelector(".worm");
 
+  let aboutMeSection = document.querySelector(".about-me-container");
+  let projectsSection = document.querySelector(".projects-container");
+  let homeSection = document.querySelector(".presentation");
+  let contactSection = document.querySelector(".contact");
+  let container = document.querySelector(".container");
+
+  projectButton.addEventListener("click", () => {
+    console.log("clicked on project");
+    wormsProject.classList.remove("hidden");
+    homeSection.classList.add("hidden");
+    aboutMeSection.classList.add("hidden");
+    contactSection.classList.add("hidden");
+    projectsSection.classList.add("hidden");
+    container.classList.add("project-holder");
+    window.scrollTo(0, 0);
+  });
 }
 
-setScrollToAbout();
+setUpProjectButton();
 setUpName();
 setUpButtons();
